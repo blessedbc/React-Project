5d56870038375c01b385fb43406aaf1a84e8c615
ikenna nwaka <inwaka39@gmail.com>
2025-06-03 16:37:44 +0200

    Update

--- src/App.jsx
+++ src/App.jsx
@@ -16,7 +16,7 @@ import EditJobPage from './pages/EditJobPage';
 
 const App = () => {
   const addJob = async (newJob) => {
-    const res = await fetch('/api/jobs', {
+    const response = await fetch('/api/jobs', {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json',         
