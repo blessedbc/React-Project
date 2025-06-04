8254b04119f289a22489e08ad8febe1dc416eaa8
ikenna nwaka <inwaka39@gmail.com>
2025-06-04 08:54:45 +0200

    first commit

--- src/pages/JobPage.jsx
+++ src/pages/JobPage.jsx
@@ -54,11 +54,9 @@ const JobPage = ( { deleteJob } ) => {
               <h3 className="text-indigo-800 text-lg font-bold mb-6">
                 Job Description
               </h3>
-
               <p className="mb-4">
                {job.description}
               </p>
-
               <h3 className="text-indigo-800 text-lg font-bold mb-2">Salary</h3>
 
               <p className="mb-4">{job.salary} / Year</p>
