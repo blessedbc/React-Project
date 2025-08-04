bdcd3091683b4de651293a93985a2e899a776016
ikenna nwaka <inwaka39@gmail.com>
2025-08-04 22:11:00 +0200

    update

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -4,6 +4,7 @@ const Hero = () => {
   return (
        <section className="bg-indigo-700 py-20 mb-4">
       <div
+      
         className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 flex flex-col items-center"
       >
         <div className="text-center">
@@ -16,6 +17,7 @@ const Hero = () => {
             Find the React job that fits your skills and needs
           </p>
         </div>
+
       </div>
     </section> 
   )
