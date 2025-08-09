7e85f58c0c9f3fbb221850e55ccb2f25aba3bbfb
ikenna nwaka <inwaka39@gmail.com>
2025-08-09 19:49:18 +0200

    AddJobPage

--- src/pages/AddJobPage.jsx
+++ src/pages/AddJobPage.jsx
@@ -206,7 +206,6 @@ const AddJobPage = ( {addJobSubmit} ) => {
                 onChange={ (e) => setContactPhone(e.target.value) }
               />
             </div>
-
             <div>
               <button
                 className="bg-indigo-500 hover:bg-indigo-600 text-white font-bold py-2 px-4 rounded-full w-full focus:outline-none focus:shadow-outline"
