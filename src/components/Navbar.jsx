9a97dbf9ccc6cd32e70ac6ed96e64abc82f87b75
ikenna nwaka <inwaka39@gmail.com>
2025-08-12 19:16:16 +0200

    update

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -7,7 +7,6 @@ const Navbar = () => {
     isActive
   ? 'bg-black text-white hover:bg-gray-900 hover:text-white rounded-md px-3 py-2'
   : 'black text-white hover:bg-gray-900 hover:text-white rounded-md px-3 py-2'
-
   return (
     <nav className="bg-indigo-700 border-b border-indigo-500">
       <div className="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
