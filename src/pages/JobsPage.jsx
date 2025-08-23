48a8d770b2b8bca3d9f7df26420b3407b4281495
Ikenna Benedict Nwaka <inwaka39@gmail.com>
2025-08-23 20:44:12 +0200

    jobs

--- src/pages/JobsPage.jsx
+++ src/pages/JobsPage.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import JobListings from '../components/JobListings'
 
 const JobsPage = () => {
+  
   return (
     <section className='bg-blue-50 px-4 py-6'>
       <JobListings />
