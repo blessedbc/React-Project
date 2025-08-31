928a1b3df05d6d710c4529ec46b04b4c5d925712
Ikenna Benedict Nwaka <inwaka39@gmail.com>
2025-08-31 20:49:29 +0200

    update

--- src/components/HomeCards.jsx
+++ src/components/HomeCards.jsx
@@ -6,7 +6,7 @@ const HomeCards = () => {
 
   return (
           <section className="py-4">
-            
+
       <div className="container-xl lg:container m-auto">
         <div className="grid grid-cols-1 md:grid-cols-2 gap-4 p-4 rounded-lg">
           <Card>
@@ -39,6 +39,7 @@ const HomeCards = () => {
         </div>
       </div>
     </section>
+    
   )
 }
 
