42a4b3cde12631483aaa8554da9c5ef5cfd74883
Ikenna Benedict Nwaka <inwaka39@gmail.com>
2025-08-31 20:48:55 +0200

    section

--- src/components/HomeCards.jsx
+++ src/components/HomeCards.jsx
@@ -6,6 +6,7 @@ const HomeCards = () => {
 
   return (
           <section className="py-4">
+            
       <div className="container-xl lg:container m-auto">
         <div className="grid grid-cols-1 md:grid-cols-2 gap-4 p-4 rounded-lg">
           <Card>
@@ -26,7 +27,7 @@ const HomeCards = () => {
             <p className="mt-2 mb-4">
               List your job to find the perfect developer for the role
             </p>
-            
+
             <Link
               to="/add-job"
               className="inline-block bg-indigo-500 text-white rounded-lg px-4 py-2 hover:bg-indigo-600"
