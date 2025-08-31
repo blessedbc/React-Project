f95d019f142dfcc3807c0117fe48897046323c63
Ikenna Benedict Nwaka <inwaka39@gmail.com>
2025-08-31 20:48:35 +0200

    p

--- src/components/HomeCards.jsx
+++ src/components/HomeCards.jsx
@@ -22,10 +22,11 @@ const HomeCards = () => {
           </Card>          
           <Card bg='bg-indigo-100'>
             <h2 className="text-2xl font-bold">For Employers</h2>
-            
+
             <p className="mt-2 mb-4">
               List your job to find the perfect developer for the role
             </p>
+            
             <Link
               to="/add-job"
               className="inline-block bg-indigo-500 text-white rounded-lg px-4 py-2 hover:bg-indigo-600"
